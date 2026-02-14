efc9db54c81423d3843b4dd6660bedf12e5fc3bb
Shubhamxmax <shubh101bhagat@gmail.com>
2026-02-14 08:27:46 +0530

    Adding Learning

--- Lecture3/script.js
+++ Lecture3/script.js
@@ -3,6 +3,35 @@
 console.dir(window.document.body.childNodes[0])
 console.log(window.document.body)
 
-let var1=document.getElementById("heading") //accessing element through id
+// Accessing elements of HTML element through JavaScript
+
+let var1=document.getElementById("para") //accessing element through id
 console.log(var1) // showing content as it is in html
 console.dir(var1) // showing content as object variable
+
+
+let headings = document.getElementsByClassName("heading") //accessing element through class name in html
+console.dir(headings)
+console.log(headings)
+
+let buttons=document.getElementsByTagName("button") //accessing element through tag name
+console.dir(buttons)
+console.log(buttons)
+
+// Accessing Element through QuerySelector
+
+let first=document.querySelector("p") // tag
+console.dir(first)
+
+let all=document.querySelectorAll("p") 
+console.dir(all)
+
+
+let first1=document.querySelector(".heading") // class
+console.dir(first1)
+
+let all1=document.querySelectorAll(".heading") 
+console.dir(all1)
+
+let para=document.querySelector("#para") // para
+console.dir(para)
